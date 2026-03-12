7bb0df77102c9749b162155a0ba0aac61926ff23
Denis12311 <denispriym1@gmail.com>
2026-03-12 12:38:47 +0200

    first commit

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskApi.Models;
+
+
+namespace TaskApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : Controller
+    {
+
+        public static List<TaskItem> tasks = new List<TaskItem>();
+        private static int nextId = 1;
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var task = tasks.FirstOrDefault(x => x.Id == id);
+            if (task == null) return NotFound();
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public IActionResult Post(TaskItem task)
+        {
+            task.Id=nextId++;
+            task.IsCompleted = false;
+            tasks.Add(task);
+            return CreatedAtAction(nameof(GetById), new { id = task.Id }, task);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutById(int id,TaskItem task)
+        {
+            var taskUpdated=tasks.FirstOrDefault(x=>x.Id==id);
+            if (taskUpdated == null) return NotFound();
+
+            taskUpdated.Title =task.Title;
+            taskUpdated.IsCompleted=task.IsCompleted;
+
+           
+            return Ok(taskUpdated);
+        }
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteById(int id)
+        {
+            var task = tasks.FirstOrDefault(x => x.Id == id);
+            if (task == null) return NotFound();
+            tasks.Remove(task);
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchById(int id)
+        {
+            var task = tasks.FirstOrDefault(x => x.Id == id);
+            if (task == null) return NotFound();
+            task.IsCompleted = !task.IsCompleted;
+            
+
+            return Ok(task);
+
+        }
+
+
+
+
+    }
+}
